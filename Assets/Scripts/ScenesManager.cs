e1e257fbe39dd2c4eda410a1fd478298d84bb35e
leh-torres <leticiatp2@gmail.com>
2023-06-30 14:35:53 -0300

    Tela Inicial quase finalizada

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public enum Scene
+    {
+        TelaInicial,
+        TelaEscolhaPersonagem,
+        GameMap
+    }
+
+    public void carregaCena(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    public void carregaNovoJogo()
+    {
+        SceneManager.LoadScene(Scene.GameMap.ToString());
+    }
+
+    public void carregaProximaCena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void carregaMenu()
+    {
+        SceneManager.LoadScene(Scene.TelaInicial.ToString());
+    }
+}
