72d0423f54751bfa481e391572dcd0cafbf44f35
Gabriel Rudan <84931636+gabrielrudan@users.noreply.github.com>
2023-07-01 12:45:46 -0300

    Adicionando os audios de pegar as armas

--- Assets/Scripts/Armas/WeaponManager.cs
+++ Assets/Scripts/Armas/WeaponManager.cs
@@ -13,14 +13,45 @@ public class WeaponManager : MonoBehaviour
     [SerializeField]
     public WeaponDataSO equippedWeapon;
 
-    //[SerializeField]
-    //AudioSource pickUpSound;
+    public List<AudioClip> weaponsPickUp;
+    public AudioSource weaponPickUp;
+
 
     public void EquipWeapon(WeaponDataSO weaponData)
     {
-        //pickUpSound.Play();
         
         equippedWeapon = weaponData;
+
+        if(equippedWeapon.weaponName == "Pistola")
+        {
+            weaponPickUp.clip = weaponsPickUp[1];
+        }
+        else if(equippedWeapon.weaponName == "Escopeta" || equippedWeapon.weaponName == "Espingarda")
+        {
+            weaponPickUp.clip = weaponsPickUp[5];
+        }
+        else if(equippedWeapon.weaponName == "Submetralhadora" || equippedWeapon.weaponName == "Nova")
+        {
+            weaponPickUp.clip = weaponsPickUp[3];
+        }
+        else if(equippedWeapon.weaponName == "Metralhadora")
+        {
+            weaponPickUp.clip = weaponsPickUp[2];
+        }
+        else if(equippedWeapon.weaponName == "Ray")
+        {
+            weaponPickUp.clip = weaponsPickUp[4];
+        }
+        else if(equippedWeapon.weaponName == "Explosion")
+        {
+            weaponPickUp.clip = weaponsPickUp[0];
+        }
+        else
+        {
+            weaponPickUp.clip = null;
+        }
+
+        weaponPickUp.Play();
 
         if(currentWeapon != null)
         {
