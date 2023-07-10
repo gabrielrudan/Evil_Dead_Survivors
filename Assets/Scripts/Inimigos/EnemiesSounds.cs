4b87e2f46a56ab55d8067e0dae0382c71e2b51f1
Gabriel Rudan <84931636+gabrielrudan@users.noreply.github.com>
2023-07-10 09:01:42 -0300

    Iluminação funcionando

--- Assets/Scripts/Inimigos/EnemiesSounds.cs
+++ Assets/Scripts/Inimigos/EnemiesSounds.cs
@@ -10,14 +10,26 @@ public class EnemiesSounds : MonoBehaviour
     private float random;
 
     private void Start() {
-        List<float> timers = new List<float>() {0.130f, 1.30f, 2.130f, 2.223f, 1.45f, 0.125f, 0.25f, 0.5f, 
-        0.75f, 1.0f, 1.125f, 1.25f, 1.5f, 1.75f, 2.0f, 2.125f, 2.25f, 2.5f};
+        List<float> timers = new List<float>() {3.130f, 1.30f, 2.130f, 2.223f, 1.45f, 3.125f, 3.25f, 3.5f, 
+        3.75f, 1.0f, 1.125f, 1.25f, 1.5f, 1.75f, 2.0f, 2.125f, 2.25f, 2.5f};
+
+        List<int> numbers = new List<int>() {1, 2, 3};
 
         Random rnd = new Random();
-        int randIndex = rnd.Next(timers.Count);
-        random = timers[randIndex];
+        int randIndex = rnd.Next(numbers.Count);
 
-        StartCoroutine(Wait(random));
+        if(numbers[randIndex] == 1)
+        {
+            randIndex = rnd.Next(timers.Count);
+            random = timers[randIndex];
+
+            StartCoroutine(Wait(random));
+        }
+        else
+        {
+            audioSource.enabled = false;
+        }
+
     }
 
     IEnumerator Wait(float seconds)
